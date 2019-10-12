fbe3dbe81f771e57ee15f3fb9fd475757c974b4e
mahfuz11 <mahfuzur.rahman@bs-23.net>
2019-10-13 03:37:05 +0600

    [master] refctoring two done separate method from application

--- app.py
+++ app.py
@@ -1,28 +1,9 @@
-from flask import Flask, render_template, request
-import json
+from flask import Flask, render_template, request, abort
+from data import add_meme, get_meme, load_memes
 
 app = Flask(__name__)
 
-memes = []
 
-def load_memes():
-    with open('memes.json', 'r') as f:
-        memes.extend(json.load(f))
-
-def save_memes():
-    with open('memes.json', 'w') as f:
-        json.dump(memes, f)
-
-def get_meme(meme_id: int):
-    return memes[meme_id]
-
-def add_meme(first_line: str, last_line: str):
-    memes.append({
-            'first_line': first_line,
-            'last_line': last_line
-        })
-    # save the memes in json files
-    save_memes()
 
 @app.route('/', methods=['GET', 'POST'])
 def make_meme():
@@ -31,7 +12,6 @@ def make_meme():
 
     if request.method == 'POST':
         add_meme(first_line, last_line)
-    print(memes)
 
     return render_template('base.html',
                            name="MRK",
@@ -40,7 +20,10 @@ def make_meme():
 
 @app.route('/view/<int:meme_id>')
 def view_meme(meme_id):
-    meme = get_meme(int(meme_id))
+    try:
+        meme = get_meme(int(meme_id))
+    except IndexError:
+        abort(404)
     return render_template('view.html',
                            first_line=meme.get('first_line'),
                            last_line=meme.get('last_line'))
---
+++ data.py
@@ -0,0 +1,24 @@
+import json, os
+
+memes = []
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+filename = os.path.join(BASE_DIR, 'memes.json')
+
+def load_memes():
+    with open(filename, 'r') as f:
+        memes.extend(json.load(f))
+
+def save_memes():
+    with open(filename, 'w') as f:
+        json.dump(memes, f)
+
+def get_meme(meme_id: int):
+    return memes[meme_id]
+
+def add_meme(first_line: str, last_line: str):
+    memes.append({
+            'first_line': first_line,
+            'last_line': last_line
+        })
+    # save the memes in json files
+    save_memes()
